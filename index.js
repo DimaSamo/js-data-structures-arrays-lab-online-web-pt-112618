3b552abd12c6769af14061a7d494d776ce90fcd2
Dmitri Samotessov <dimasamo96@gmail.com>
2019-07-10 20:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Write your solution here!
 let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name)
+}
